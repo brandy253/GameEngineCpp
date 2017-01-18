96e9a74f46e21081656f6346ac0123fb31fb6dde
Long <phocode7@gmail.com>
2017-01-18 16:05:31 +0700

    some fucking thing: rotate, move (both x, y and z-axes), but rotation is still glitchy

--- main.cpp
+++ main.cpp
@@ -25,9 +25,23 @@ mat4 rotationMatrix;
 mat4 projectionMatrix;
 mat4 translationMatrix;
 
+bool key_W = false;
+bool key_A = false;
+bool key_S = false;
+bool key_D = false;
+bool key_UP = false;
+bool key_DOWN = false;
+
 float dx = 0.0f;
 float dy = 0.0f;
 float dz = 0.0f;
+float x = 0.0f;
+float y = 0.0f;
+float z = 0.0f;
+
+float rotX = 0.0f;
+float rotY = 0.0f;
+float rotZ = 0.0f;
 
 GLfloat angle = 0.0f;
 
@@ -35,17 +49,53 @@ GLfloat FOV = 70;
 GLfloat NEAR_PLANE = 0.1f;
 GLfloat FAR_PLANE = 1000.0f;
 
+static const GLfloat vertices[36][3] = {
+        //  front-face
+        {-0.5,-0.5, -1.0},{-0.5, 0.5, -1.0},{ 0.5,-0.5, -1.0},
+        { 0.5,-0.5, -1.0},{-0.5, 0.5, -1.0},{ 0.5, 0.5, -1.0},
+
+        //  back-face
+        {-0.5,-0.5, -2.0},{-0.5, 0.5, -2.0},{ 0.5,-0.5, -2.0},
+        { 0.5,-0.5, -2.0},{-0.5, 0.5, -2.0},{ 0.5, 0.5, -2.0},
+
+        //  left-face
+        {-0.5,-0.5,-1.0},{-0.5, 0.5,-1.0},{-0.5, 0.5,-2.0},
+        {-0.5,-0.5,-1.0},{-0.5, 0.5,-2.0},{-0.5,-0.5,-2.0},
+
+        //  right-face
+        {0.5,-0.5,-1.0},{0.5, 0.5,-1.0},{0.5,0.5,-2.0},
+        {0.5,-0.5,-1.0},{0.5, 0.5,-2.0},{0.5,-0.5,-2.0},
+
+        //  up-face
+        {-0.5,-0.5,-1.0},{0.5,-0.5,-1.0},{-0.5,-0.5,-2.0},
+        { 0.5,-0.5,-1.0},{-0.5,-0.5,-2.0},{0.5,-0.5,-2.0},
+
+        //  down-face
+        {-0.5,0.5,-1.0},{0.5,0.5,-1.0},{-0.5,0.5,-2.0},
+        { 0.5,0.5,-1.0},{-0.5,0.5,-2.0},{0.5,0.5,-2.0}
+    };
+
+static const GLfloat colors[36][3] = {
+    { 1.0, 0.0, 0.0},{ 1.0, 0.0, 0.0},{ 1.0, 0.0, 0.0},
+    { 1.0, 0.0, 0.0},{ 1.0, 0.0, 0.0},{ 1.0, 0.0, 0.0},
+
+    { 0.0, 1.0, 0.0},{ 0.0, 1.0, 0.0},{ 0.0, 1.0, 0.0},
+    { 0.0, 1.0, 0.0},{ 0.0, 1.0, 0.0},{ 0.0, 1.0, 0.0},
+
+    { 0.0, 0.0, 1.0},{ 0.0, 0.0, 1.0},{ 0.0, 0.0, 1.0},
+    { 0.0, 0.0, 1.0},{ 0.0, 0.0, 1.0},{ 0.0, 0.0, 1.0},
+
+    { 1.0, 1.0, 0.0},{ 1.0, 1.0, 0.0},{ 1.0, 1.0, 0.0},
+    { 1.0, 1.0, 0.0},{ 1.0, 1.0, 0.0},{ 1.0, 1.0, 0.0},
+
+    { 0.0, 1.0, 1.0},{ 0.0, 1.0, 1.0},{ 0.0, 1.0, 1.0},
+    { 0.0, 1.0, 1.0},{ 0.0, 1.0, 1.0},{ 0.0, 1.0, 1.0},
+
+    { 1.0, 0.0, 1.0},{ 1.0, 0.0, 1.0},{ 1.0, 0.0, 1.0},
+    { 1.0, 0.0, 1.0},{ 1.0, 0.0, 1.0},{ 1.0, 0.0, 1.0}
+};
+
 void init() {
-
-    static const GLfloat vertices[6][3] = {
-        {-1.0,-1.0, 0.0},{-1.0, 1.0, 0.0},{ 1.0,-1.0, 0.0},
-        { 1.0,-1.0, 0.0},{-1.0, 1.0, 0.0},{ 1.0, 1.0, 0.0},
-    };
-
-    static const GLfloat colors[6][3] = {
-        { 1.0, 0.0, 0.0},{ 0.0, 1.0, 0.0},{ 0.0, 0.0, 1.0},
-        { 1.0, 1.0, 0.0},{ 1.0, 0.0, 1.0},{ 0.0, 1.0, 1.0}
-    };
 
     //  create 2 VBOs for position and color
     glGenBuffers(2, VBOID);
@@ -53,10 +103,10 @@ void init() {
     //  populate the for position buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBOID[0]);
 
-    glBufferData(GL_ARRAY_BUFFER, 3 * 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3 * 36 * sizeof(float), vertices, GL_STATIC_DRAW);
     //  populate the color buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBOID[1]);
-    glBufferData(GL_ARRAY_BUFFER, 3 * 6 * sizeof(float), colors, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3 * 36 * sizeof(float), colors, GL_STATIC_DRAW);
 
     //  create the vao
     glGenVertexArrays(1, &VAOID);
@@ -90,27 +140,26 @@ void init() {
     projectionMatrix[2][3] = -1;
     projectionMatrix[3][2] = -((2 * NEAR_PLANE * FAR_PLANE) / frustum_length);
     projectionMatrix[3][3] = 0.0f;
-
-    //cout << projectionMatrix[0][0] << " " << projectionMatrix[0][1] << " " << projectionMatrix[0][2] << " " << projectionMatrix[0][3] << endl;
-    //cout << projectionMatrix[1][0] << " " << projectionMatrix[1][1] << " " << projectionMatrix[1][2] << " " << projectionMatrix[1][3] << endl;
-    //cout << projectionMatrix[2][0] << " " << projectionMatrix[2][1] << " " << projectionMatrix[2][2] << " " << projectionMatrix[2][3] << endl;
-    //cout << projectionMatrix[3][0] << " " << projectionMatrix[3][1] << " " << projectionMatrix[3][2] << " " << projectionMatrix[3][3] << endl;
 }
 
 void display() {
-    static const float black[] = { 0.7f, 0.7f, 0.7f, 0.0f };
+    glEnable(GL_DEPTH_TEST);
+
+    static const float black[] = { 0.0f, 0.3f, 0.0f, 0.0f };
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearBufferfv(GL_COLOR, 0, black);
 
     //  Do stuff with translation
-    translationMatrix = glm::translate(mat4(1.0f), vec3(dx, dy, dz));
+    translationMatrix = glm::translate(mat4(1.0f), vec3(x, y, z));
     GLuint translationMatrixLocation = glGetUniformLocation(shader->getProgramHandle(), "TranslationMatrix");
     if(translationMatrixLocation >= 0) {
         glUniformMatrix4fv(translationMatrixLocation, 1, GL_FALSE, &translationMatrix[0][0]);
     }
 
     //  Do stuff with rotation
-    rotationMatrix = glm::rotate(mat4(1.0f), angle, vec3(0.0f, 1.0f, 0.0f));
-    //angle = (angle + 0.001f);
+    rotationMatrix = glm::rotate(mat4(1.0f), angle, vec3(1.0f, 0.0f, 0.0f));
+
+    angle = (angle + 0.001f);
     if(angle >= 360.0f) angle = 0.0f;
 
     GLuint location = glGetUniformLocation(shader->getProgramHandle(), "RotationMatrix");
@@ -125,33 +174,47 @@ void display() {
     }
 
     glBindVertexArray(VAOID);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    switch(key) {
-case GLFW_KEY_A:
-    dx -= 0.01f;
-    break;
-case GLFW_KEY_D:
-    dx += 0.01f;
-    break;
-case GLFW_KEY_W:
-    dy += 0.01f;
-    break;
-case GLFW_KEY_S:
-    dy -= 0.01f;
-    break;
-case GLFW_KEY_UP:
-    cout << "UP" << endl;
-    dz += 0.01f;
-    break;
-case GLFW_KEY_DOWN:
-    cout << "DOWN" << endl;
-    dz -= 0.01f;
-    break;
-default:
-    break;
+    if(key == GLFW_KEY_ESCAPE) exit(0);
+
+    bool pressed = ( action == GLFW_PRESS );
+
+    if(action == GLFW_PRESS) {
+        if(key == GLFW_KEY_A)   dx = -0.01f;
+        if(key == GLFW_KEY_D)   dx = 0.01f;
+        if(key == GLFW_KEY_W)   dy = 0.01f;
+        if(key == GLFW_KEY_S)   dy = -0.01f;
+        if(key == GLFW_KEY_UP)  dz = 0.01f;
+        if(key == GLFW_KEY_DOWN)dz = -0.01f;
+    } else if(action == GLFW_RELEASE) {
+        if(key == GLFW_KEY_A)   dx = 0.0f;
+        if(key == GLFW_KEY_D)   dx = 0.0f;
+        if(key == GLFW_KEY_W)   dy = 0.0f;
+        if(key == GLFW_KEY_S)   dy = 0.0f;
+        if(key == GLFW_KEY_UP)  dz = 0.0f;
+        if(key == GLFW_KEY_DOWN)dz = 0.0f;
+    }
+}
+
+void moving() {
+    y += dy;
+    x += dx;
+    z += dz;
+}
+
+void rotating() {
+
+}
+
+void debug() {
+    for(int i = 0 ; i < 6 ; i++) {
+        cout << "{";
+        for(int j = 0 ; j < 3 ; j++)
+            cout << vertices[i][j] << ",";
+        cout << "}" << endl;
     }
 }
 
@@ -186,6 +249,7 @@ int main()
 
     //  Main loop
     while(!glfwWindowShouldClose(window)) {
+        moving();
         display();
         glfwSwapBuffers(window);
         glfwPollEvents();
--- triangles.vert
+++ triangles.vert
@@ -8,8 +8,9 @@ out vec3 Color;
 uniform mat4 RotationMatrix;
 uniform mat4 ProjectionMatrix;
 uniform mat4 TranslationMatrix;
+uniform mat4 TransformedMatrix;
 
 void main() {
     Color = VertexColor;
-    gl_Position = ProjectionMatrix * TranslationMatrix * RotationMatrix * vec4(VertexPosition, 1.0);
+    gl_Position = ProjectionMatrix * RotationMatrix * TranslationMatrix *  vec4(VertexPosition, 1.0);
 }
